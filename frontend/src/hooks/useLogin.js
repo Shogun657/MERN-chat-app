52e604fe390f80d744d733513780c0addc51b2e4
Shogun657 <shaanjain26jun@gmail.com>
2024-05-31 13:23:52 +0530

    added auth

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { useAuthContext } from "../context/AuthContext";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { setAuthUSer } = useAuthContext();
+
+
+
+  const login = async (username, password) => {
+    const success = handleInputErrors(username, password);
+    console.log(success);
+    if (!success) return;
+    setLoading(true)
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-type": "applications/json" },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUSer(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return { loading, login };
+}
+
+export default useLogin
+
+function handleInputErrors(username, password) {
+  if (!username || !password) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+
+  return true;
+}
